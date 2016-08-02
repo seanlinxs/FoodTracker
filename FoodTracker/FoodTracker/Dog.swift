9722866c288d76dc3f403e710ac8c306bb7151ae
Sean Lin <slin@dius.com.au>
2016-08-02 16:29:50 +1000

    Create new model - Dog

---
+++ FoodTracker/FoodTracker/Dog.swift
@@ -0,0 +1,15 @@
+//
+//  Dog.swift
+//  FoodTracker
+//
+//  Created by Sean Lin on 2/08/2016.
+//  Copyright © 2016 DiUS. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Dog: Object {
+	dynamic var name = ""
+	dynamic var age = 0
+}
